9e57bfe22914418d40c97bda3f015ccdb10db836
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-17 22:29:06 +0530

    OmniFood Project Completed

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// Make Mobie Navigation Work
+
+const buttoNavEl = document.getElementById('btn-mobile-nav');
+const headerEl = document.getElementById('header');
+buttoNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const heroEl = document.querySelector('.section--hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-100px',
+  }
+);
+obs.observe(heroEl);
+///////////////////////////////////////////////////////////
+// Smooth Scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll to back
+    if (href === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+    //  Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close Mobile Nav
+
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
+
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  // console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
+
+*/
